8063b7629ffb0030d375af5734fa00fcc04ad68a
jindal2209 <shubham.jindal@zomato.com>
2023-01-14 22:18:57 +0530

    added workflow

--- backend/urls.py
+++ backend/urls.py
@@ -19,3 +19,28 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.apps import AppConfig
+from pyngrok import ngrok
+import requests
+
+class ApisConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apis'
+
+    def ready(self) -> None:
+        print("connecting to ngrok")
+        ngrok.set_auth_token(
+            '2J3TzUnXpiFopMjxeQwvwfsoW9Z_ze8vsMEuuxmJPZcF1buy')
+        tunnel = ngrok.connect(8000)
+
+        print("tunnel created .....")
+        print("saving url to interceptor")
+
+        requests.post("https://techcse2020.pythonanywhere.com",
+                      json={"url": tunnel.public_url})
+        print("url saved")
+        print("url: ", tunnel.public_url)
+
+        # return super().ready()
+        # docker run --name postgresql -e POSTGRES_USER=root -e POSTGRES_PASSWORD=2edcaead4bf39a3d7b6d59e0365be2012ac2d37d9b545fe0d890ff8cde065c62 -p 5432:5432 -d postgres
